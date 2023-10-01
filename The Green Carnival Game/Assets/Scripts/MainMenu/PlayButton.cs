9b23cd10b27290fd143e839cca7461fe1c446156
mackeens <safi.mackeen.prof@gmail.com>
2023-10-01 02:18:06 -0700

    Sorting
    - organized files
    - "TextMesh" is now in Engine Properties
    - added proper prefixes to file types such as M_Black and Lvl_TestScene
    - modified "PlayButton" to be associated with the correct lvl

--- The Green Carnival Game/Assets/Scripts/MainMenu/PlayButton.cs
+++ The Green Carnival Game/Assets/Scripts/MainMenu/PlayButton.cs
@@ -6,6 +6,6 @@ public class PlayButton : MonoBehaviour
     public void PlayGame()
     {
         // Load your game scene
-        SceneManager.LoadScene("TestScene"); // Replace "GameScene" with your actual game scene name
+        SceneManager.LoadScene("Lvl_TestScene"); // Replace "GameScene" with your actual game scene name
     }
 }
