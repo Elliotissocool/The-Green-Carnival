fec3f41f0df70be9d26d5625eb25245c9fe2e8f3
eqwisp <eacrisp@ucsc.edu>
2023-10-15 15:17:12 -0700

    Added Gate Opener

--- The Green Carnival Game/Assets/Scripts/Interaction/GateOpener.cs
+++ The Green Carnival Game/Assets/Scripts/Interaction/GateOpener.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class GateOpener : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string ticketTag = "Ticket"; // Tag for the ticket object.
+    public Transform gateTransform; // Reference to the gate's Transform.
+    public float openAngle = 90.0f; // Angle by which the gate should open.
+
+    private bool isOpen = false;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag(ticketTag) && !isOpen)
+        {
+            // Ticket entered the gate's trigger area.
+            OpenGate();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OpenGate()
     {
-        
+        // Rotate the gate to open it.
+        gateTransform.Rotate(Vector3.up, openAngle);
+        isOpen = true;
+
+        // You can also play a gate opening sound or trigger any other effects here.
     }
 }
